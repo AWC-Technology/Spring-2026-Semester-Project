3a7deb5126ee0203c2d4e137155ddbcd3c0fcac6
natibbet25 <nzt5245@psu.edu>
2026-04-13 10:42:34 -0400

    Add files via upload

---
+++ frontend/course_compass/postcss.config.mjs
@@ -0,0 +1,14 @@
+import { Config } from 'tailwindcss'
+
+const config = {
+  darkMode: 'class',
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+export default config
